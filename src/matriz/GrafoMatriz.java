377f53b275a1edaaffb2ccf188c1cb3f6c4e1497
jdvega-dev <jvegaq21@campusuninunez.edu.co>
2026-02-16 20:29:08 -0500

    Refactor GrafoMatriz to use edges instead of vertices

--- src/matriz/GrafoMatriz.java
+++ src/matriz/GrafoMatriz.java
@@ -1,68 +1,29 @@
-import java.util.ArrayList;
+package matriz;
 
 public class GrafoMatriz {
 
-    private ArrayList<String> vertices;
     private int[][] matriz;
-    private int capacidad;
+    private int tamaño;
 
     public GrafoMatriz(int tamaño) {
-        capacidad = tamaño;
-        vertices = new ArrayList<>();
+        this.tamaño = tamaño;
         matriz = new int[tamaño][tamaño];
     }
 
-    public void agregarVertice(String vertice) {
+    public void agregarArista(int origen, int destino) {
+        if (origen >= 0 && origen < tamaño &&
+            destino >= 0 && destino < tamaño) {
 
-        if (vertices.contains(vertice)) {
-            System.out.println("El vértice " + vertice + " ya existe.");
-            return;
-        }
+            matriz[origen][destino] = 1;
 
-        if (vertices.size() >= capacidad) {
-            System.out.println("No se pueden agregar más vértices.");
-            return;
-        }
-
-        vertices.add(vertice);
-        System.out.println("Vértice " + vertice + " agregado correctamente.");
-    }
-
-    public void agregarArista(String origen, String destino) {
-
-        int i = vertices.indexOf(origen);
-        int j = vertices.indexOf(destino);
-
-        if (i == -1 || j == -1) {
+        } else {
             System.out.println("Error: Uno o ambos vértices no existen.");
-            return;
         }
-
-        if (matriz[i][j] == 1) {
-            System.out.println("La arista ya existe.");
-            return;
-        }
-
-        matriz[i][j] = 1; // Dirigido
-        System.out.println("Arista agregada: " + origen + " -> " + destino);
     }
 
     public void mostrarMatriz() {
-
-        if (vertices.isEmpty()) {
-            System.out.println("El grafo está vacío.");
-            return;
-        }
-
-        System.out.print("  ");
-        for (String v : vertices) {
-            System.out.print(v + " ");
-        }
-        System.out.println();
-
-        for (int i = 0; i < vertices.size(); i++) {
-            System.out.print(vertices.get(i) + " ");
-            for (int j = 0; j < vertices.size(); j++) {
+        for (int i = 0; i < tamaño; i++) {
+            for (int j = 0; j < tamaño; j++) {
                 System.out.print(matriz[i][j] + " ");
             }
             System.out.println();
